e6967f9572230e69e6f7b31b6ce11683c46f6879
SoldatdOR05 <ethan.paleyron@gmail.com>
2023-04-15 17:22:15 +0200

    dark mode button

--- js/components/_dark_mode.js
+++ js/components/_dark_mode.js
@@ -1,18 +1,21 @@
 const darkMode = document.getElementById('darkMode');
 let bool = true;
 
-darkMode.addEventListener('click', () => {
+darkMode.addEventListener('click', (colorButtonDark, colorButtonLight) => {
+    colorButtonLight = "#EAEBEB";
+    colorButtonDark = "#2D3142";
+
     if (bool) {
+        document.body.dataset["theme"] = "dark";
         darkMode.innerHTML = "light";
         darkMode.style.color = "#000";
-        darkMode.style.backgroundColor = "#EAEBEB";
-
-
+        darkMode.style.backgroundColor = colorButtonLight;
         bool = false;
     } else {
+        document.body.dataset["theme"] = "light";
         darkMode.innerHTML = "dark";
         darkMode.style.color = "#fff";
-        darkMode.style.backgroundColor = "#2D3142";
+        darkMode.style.backgroundColor = colorButtonDark;
         bool = true;
     }
 })
