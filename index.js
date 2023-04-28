a234c6012d67c2ea56ca3b6961890fead9cc12e2
SoldatdOR05 <epaleyron@edenschool.fr>
2023-04-28 09:05:41 +0200

    switch

---
+++ index.js
@@ -0,0 +1,29 @@
+// 🔴Dark Mode
+const darkMode = document.getElementById('darkMode');
+let bool = true;
+
+darkMode.addEventListener('click', () => {
+    if (bool) {
+        document.body.dataset["theme"] = "dark";
+        darkMode.src = "./img/logo/circle-light.svg"
+        bool = false;
+    } else {
+        document.body.dataset["theme"] = "light";
+        darkMode.src = "./img/logo/circle-dark.svg"
+        bool = true;
+    }
+})
+
+// 🔴Switch
+const switchImg = document.getElementsByClassName('switchImg');
+let b = true;
+
+switchImg.addEventListener('click', () => {
+    if (b) {
+        // switchImg.src = "./img/logo/circle-light.svg"
+        b = false;
+    } else {
+        // switchImg.src = "./img/logo/circle-light.svg"
+        b = true;
+    }
+})
